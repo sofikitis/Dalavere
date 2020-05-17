b458a1f9f55d4f11dab019bd3e1d3a3b6353d387
GiorgosFradelos <64437718+GiorgosFradelos@users.noreply.github.com>
2020-05-17 21:14:56 +0300

    Add files via upload

---
+++ obj/Deliverer.java
@@ -0,0 +1,18 @@
+package com.example.myapplication.obj;
+
+class Deliverer extends User {
+    String status;
+    Availability availability_obj;
+
+
+    public Deliverer(Availability availability_obj){
+        status = "Available";
+        this.availability_obj = availability_obj;
+    }
+
+
+    void acceptRequest(){}
+
+    void declineRequest(){}
+
+}
