93f8bbb93d48a3c105d3fa00945b0f519a36c59f
GiorgosFradelos <64437718+GiorgosFradelos@users.noreply.github.com>
2020-05-31 18:39:38 +0300

    Update Wallet.java

--- Project Code v0.4/myapplication/obj/Wallet.java
+++ Project Code v0.4/myapplication/obj/Wallet.java
@@ -18,6 +18,42 @@ public class Wallet {
     void addTransaction(Transaction transaction_obj){
         transaction_history.add(transaction_obj);
     }
+    
+     public void chargeWallet(){
+        // get card info
+        // request transaction
+
+        /*
+        if (status.equals("Successful")){
+            update balance
+        }else{
+            show card denied message
+        }
+        */
+    }
+
+    public void unChargeWallet(){
+        // get card info
+        // request transaction
+
+        /*
+        if (status.equals("Successful")){
+            update balance
+        }else{
+            show error message
+        }
+        */
+    }
+
+    public void completeOrderTransaction(Transaction trans){
+        // transfer trans.final_cost from trans.receiver_obj to trans.deliverer_obj
+    }
+
+    public void createTranscaction(Ongoing_Order ord, double f_cost){
+
+        Transaction trans = new Transaction(ord.receiver_obj, ord.deliverer_obj, f_cost);
+
+    }
 }
 
 
